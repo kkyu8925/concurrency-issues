c69a62d444b2f89ff00b756b585b84f3c2aa96b5
kkyu8925 <kkyu8925@gmail.com>
2023-04-11 23:15:58 +0900

    문제점

--- src/test/kotlin/com/example/stock/service/StockServiceTest.kt
+++ src/test/kotlin/com/example/stock/service/StockServiceTest.kt
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
 
 @SpringBootTest
 class StockServiceTest @Autowired constructor(
@@ -29,5 +32,27 @@ class StockServiceTest @Autowired constructor(
         stockService.decrease(1L, 1L)
         val stock = stockRepository.findById(1L).orElseThrow()
         assertEquals(99, stock.quantity)
+    }
+
+    @Test
+    fun `동시에 100명이 주문`() {
+        val threadCount = 100
+        val executorService = Executors.newFixedThreadPool(32)
+        val latch = CountDownLatch(threadCount)
+
+        for (i in 0..threadCount) {
+            executorService.submit {
+                try {
+                    stockService.decrease(1L, 1L)
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+
+        latch.await()
+
+        val stock = stockRepository.findById(1L).orElseThrow()
+        assertEquals(0, stock.quantity)
     }
 }
